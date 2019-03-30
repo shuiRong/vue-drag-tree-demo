d0eb6787e0bfa8e06b15a807c9e4e228277e7400
shuiRong <shuirong1997@icloud.com>
2019-03-30 22:09:24 +0800

    更新依赖引用;

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import VueDragTree from './components/temp/index.js'
-// import VueDragTree from 'vue-drag-tree'
-// import 'vue-drag-tree/dist/vue-drag-tree.min.css'
+// import VueDragTree from './components/temp/index.js'
+import VueDragTree from 'vue-drag-tree'
+import 'vue-drag-tree/dist/vue-drag-tree.min.css'
 
 Vue.config.productionTip = false
 Vue.use(VueDragTree)
